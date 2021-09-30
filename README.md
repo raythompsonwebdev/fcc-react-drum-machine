a39e4d4c919b5ef69b43cc2571592608d910e833
raythompsonwebdev <ray_thomp@hushmail.com>
2021-09-30 16:00:24 +0100

    edit readme file

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 
-# fcc-react-drum-machine
+# fcc-drum-machine
 
 ![Free CodeCamp Drum Machine](/assets/fcc-drum-machine.png)
+
+Free Code Camp project built using Vanilla Javascript and React v16.8.
+
+Three versions of the same Drum machine using Vanilla Javascript and React function based and class based methods.
+
+
+## Installation
+
+Download files : 
+
+```sh
+git clone git@github.com:raythompsonwebdev/fcc-react-drum-machine.git
+```
+
+Unfortunately , You will need to add your own sound files.
+
+
+## Tools
+
+- [React Development] (https://unpkg.com/react@16.8.2/umd/react.development.js)  -  React Development CDN
+- [React Dom] (https://unpkg.com/react-dom@16.8.2/umd/react-dom.development.js) - 
+- [Standalone Babel] (https://unpkg.com/@babel/standalone/babel.min.js) - Stand Alone Babel CDN 
+
+
--- index.js
+++ index.js
@@ -3,60 +3,60 @@ window.focus();
 const drum = document.querySelectorAll(".drum-pad");
 
 const data = [
-    {
-      letter: "Q",
-      keycode: 81,
-      id: "open-hh",
-      url: "./FCC-Drum-Machine-Sounds/openhat.wav",
-    },
-    {
-      letter: "W",
-      keycode: 87,
-      id: "closed-hh",
-      url: "./FCC-Drum-Machine-Sounds/hihat.wav",
-    },
-    {
-      letter: "E",
-      keycode: 69,
-      id: "kick-and-hat",
-      url: "./FCC-Drum-Machine-Sounds/kick.wav",
-    },
-    {
-      letter: "A",
-      keycode: 65,
-      id: "boom",
-      url: "./FCC-Drum-Machine-Sounds/boom.wav",
-    },
-    {
-      letter: "S",
-      keycode: 83,
-      id: "clap",
-      url: "./FCC-Drum-Machine-Sounds/clap.wav",
-    },
-    {
-      letter: "D",
-      keycode: 68,
-      id: "ride",
-      url: "./FCC-Drum-Machine-Sounds/ride.wav",
-    },
-    {
-      letter: "Z",
-      keycode: 90,
-      id: "tink",
-      url: "./FCC-Drum-Machine-Sounds/tink.wav",
-    },
-    { 
-      letter: "X", 
-      keycode: 88, 
-      id: "Tom", 
-      url: "./FCC-Drum-Machine-Sounds/tom.wav" 
-    },
-    {
-      letter: "C",
-      keycode: 67,
-      id: "snare",
-      url: "./FCC-Drum-Machine-Sounds/snare.wav",
-    },
+  {
+    letter: "Q",
+    keycode: 81,
+    id: "open-hh",
+    url: "./FCC-Drum-Machine-Sounds/openhat.wav",
+  },
+  {
+    letter: "W",
+    keycode: 87,
+    id: "closed-hh",
+    url: "./FCC-Drum-Machine-Sounds/hihat.wav",
+  },
+  {
+    letter: "E",
+    keycode: 69,
+    id: "kick-and-hat",
+    url: "./FCC-Drum-Machine-Sounds/kick.wav",
+  },
+  {
+    letter: "A",
+    keycode: 65,
+    id: "boom",
+    url: "./FCC-Drum-Machine-Sounds/boom.wav",
+  },
+  {
+    letter: "S",
+    keycode: 83,
+    id: "clap",
+    url: "./FCC-Drum-Machine-Sounds/clap.wav",
+  },
+  {
+    letter: "D",
+    keycode: 68,
+    id: "ride",
+    url: "./FCC-Drum-Machine-Sounds/ride.wav",
+  },
+  {
+    letter: "Z",
+    keycode: 90,
+    id: "tink",
+    url: "./FCC-Drum-Machine-Sounds/tink.wav",
+  },
+  { 
+    letter: "X", 
+    keycode: 88, 
+    id: "Tom", 
+    url: "./FCC-Drum-Machine-Sounds/tom.wav" 
+  },
+  {
+    letter: "C",
+    keycode: 67,
+    id: "snare",
+    url: "./FCC-Drum-Machine-Sounds/snare.wav",
+  },
 ];
 
 const updateDisplay = (display) => {
@@ -64,22 +64,21 @@ const updateDisplay = (display) => {
 };
 
 const clickPlay = (e) => {
-    const dataKey = e.target.getAttribute("id");
-    
-    if(dataKey){
-        const sound = document.querySelector(`audio[data-key="${dataKey}"]`);        
-        const pad = document.querySelector(`div[id="${dataKey}"]`);     
-        pad.classList.add("playing");
-        sound.currentTime = 0;
-        sound.play();          
-        updateDisplay(dataKey);
-    }else{
-        return;
-    }
+  const dataKey = e.target.getAttribute("id");
+  
+  if(dataKey){
+      const sound = document.querySelector(`audio[data-key="${dataKey}"]`);        
+      const pad = document.querySelector(`div[id="${dataKey}"]`);     
+      pad.classList.add("playing");
+      sound.currentTime = 0;
+      sound.play();          
+      updateDisplay(dataKey);
+  }else{
+      return;
+  }
 };
 
-const playSound = (e) => { 
-  
+const playSound = (e) => {   
   for(element of data){
     if(e.keyCode == element.keycode){
       const audio = document.querySelector(`audio[id="${element.id}"]`);
@@ -94,8 +93,7 @@ const playSound = (e) => {
 };
 
 drum.forEach((element)=>{
-    element.addEventListener("click", clickPlay)
-    
+    element.addEventListener("click", clickPlay);    
 })
    
 document.addEventListener("keydown", playSound);
